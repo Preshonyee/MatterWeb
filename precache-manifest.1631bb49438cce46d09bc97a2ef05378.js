5d0cc6e41b4f4371c18130da55a0fed0b4c982fc
Presh Onyee <presh_onyee@outlook.com>
2020-04-15 18:43:38 +0100

    Updates

---
+++ precache-manifest.1631bb49438cce46d09bc97a2ef05378.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726323cd0382d75b814e70f3ca5d1db5",
+    "url": "/matter-web/index.html"
+  },
+  {
+    "revision": "9135212cd4460adb7361",
+    "url": "/matter-web/static/css/main.9e24c1c7.chunk.css"
+  },
+  {
+    "revision": "257a2bff6f2c1643f1f2",
+    "url": "/matter-web/static/js/2.0d34cf0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/matter-web/static/js/2.0d34cf0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9135212cd4460adb7361",
+    "url": "/matter-web/static/js/main.b6dea785.chunk.js"
+  },
+  {
+    "revision": "618dd19dac063da6a8f0",
+    "url": "/matter-web/static/js/runtime-main.7c88a2b6.js"
+  }
+]);
